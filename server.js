d70d4db7eab78f3604dc5e21a52d6c9995934b62
GODISGOOD <ltv75850@gmail.com>
2025-08-07 17:51:47 +0800

    FIRST COMMIT

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require(`express`);
+const app = express();
+const PORT = 3000
+
+app.get(`/`,(req, res) => {
+    res.send(`
+        
+        <!DOCTYPE html >
+        <html>
+            <head>
+                <style>
+
+                    body{
+                        display: flex;
+                        flex-direction:column;
+                        align-items: center;
+                        justify-items: center;
+                        margin-top: 5%;
+                    }
+                    h1{
+                        color:red;
+                        font-size: 50px;
+                    }
+                    div{
+                        display: flex;
+                        flex-direction:column;
+                        align-items: center;
+                        justify-items: center;
+                        box-shadow:3px 3px 5px #000;
+                        width: 40%;
+                    }
+                </style>
+            </head>
+            <body>
+                <div>
+                    <h1>Server under maintenance</h1>
+                    <p> We are currently performing schedule maintenance. Please check back  after. </p>
+                    <p> Thank you for  your patience</p> 
+                </div>
+            </body>
+        
+        </html>
+        
+        `);
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http//localhost:${PORT}`)
+})
+
